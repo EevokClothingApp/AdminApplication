f0f4cdca5c28156258b93aa7a297195f226ccf66
AnushkaX <anushka.mutusinghe@gmail.com>
2020-08-07 11:34:41 +0530

    manage page ui created

--- adminapplication/lib/screens/manage.dart
+++ adminapplication/lib/screens/manage.dart
@@ -3,8 +3,45 @@ import 'package:flutter/material.dart';
 class Manage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.add),
+          title: Text("Add Products"),
+          onTap: () {},
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.change_history),
+          title: Text("Products List"),
+          onTap: () {},
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.add_circle),
+          title: Text("Add Category"),
+          onTap: () {},
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.category),
+          title: Text("Category List"),
+          onTap: () {},
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.add_circle_outline),
+          title: Text("Add Brand"),
+          onTap: () {},
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.library_books),
+          title: Text("Brand List"),
+          onTap: () {},
+        ),
+        Divider(),
+      ],
     );
   }
 }
